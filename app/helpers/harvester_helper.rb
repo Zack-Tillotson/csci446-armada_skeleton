4dd19276cb2e09a5dbfbaf4b9d65cc6fc6c421b6
Kyle Schulz <ryoken@ryoken.net>
2011-04-17 20:04:50 -0600

    Ticket 29 - added more link to descriptions

--- app/helpers/harvester_helper.rb
+++ app/helpers/harvester_helper.rb
@@ -10,4 +10,12 @@ module HarvesterHelper
 		end
 	end
 
+	def more_link(h)
+		if h.description.length > 50
+			link_to_function("more", nil, :id => "1") do |page|
+				page["desc#{h.id}"].replace_html "#{h.description}"
+			end
+		end
+	end
+
 end
