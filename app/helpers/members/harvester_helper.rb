88f431aa71cfa9b7a9ebe3d2048eb0323ccc83e9
Kyle Schulz <ryoken@ryoken.net>
2011-04-18 22:24:48 -0600

    merging

--- app/helpers/members/harvester_helper.rb
+++ app/helpers/members/harvester_helper.rb
@@ -2,7 +2,7 @@ module Members::HarvesterHelper
 
 	def links(h)
 		if current_user.id==h.user_id
-			render :partial => :links
+			render :partial => 'links', :locals => {:harvester => h}
 		end
 	end
 
