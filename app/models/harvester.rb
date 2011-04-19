57c862761e6639d0eb7512145281250b70128738
Kyle Schulz <ryoken@ryoken.net>
2011-04-18 20:58:48 -0600

    Fixed show for user and admin

--- app/models/harvester.rb
+++ app/models/harvester.rb
@@ -6,5 +6,9 @@ class Harvester < ActiveRecord::Base
 	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
-	
+
+	def to_s
+		name
+	end
+
 end
